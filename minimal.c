27293435a56325a552a2c575bc3b0d27f320950c
yurong0404 <t3837735@gmail.com>
2019-12-10 12:26:29 +0800

    minimal.c

--- minimal.c
+++ minimal.c
@@ -32,16 +32,15 @@ int main(int argc, char *argv[]) {
 		ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL|PTRACE_O_TRACEEXIT);
 		ptrace(PTRACE_CONT, child, 0, 0);
 		waitpid(child, &status, 0);
-		while(1)
-		{
+		ptrace(PTRACE_CONT, child, 0, 0);
+		waitpid(child, &status, 0);	
+//		while(1)
+//		{
 		printf("pid: %d\ndata section: \n", child);
 		scanf("%lx", &addr);
 		output = ptrace(PTRACE_PEEKTEXT, child, addr, 0);
-//		if(output = ptrace(PTRACE_PEEKTEXT, child, 0x824, NULL) == -1) errquit("peektext");
 		printf("%lx\n", output);
-		}
-		ptrace(PTRACE_CONT, child, 0, 0);
-		waitpid(child, &status, 0);
+		//}
 		perror("done");
 	}
 	return 0;
